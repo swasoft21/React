60f1217558718227bec6d64e111721797548cb92
Swathi Sudhakar Devadiga <sdswathi790@gmail.com>
2026-07-20 20:24:52 +0530

    Convert home and checkout to async await

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -13,15 +13,18 @@ export function CheckoutPage({cart}) {
     const [paymentSummary,setPaymentSummary] = useState(null)    
 
     useEffect(()=>{
-        axios.get('/api/delivery-options?expand=estimatedDeviveryTime')
-            .then((response)=>{
+        const fetchChechoutData=async()=>{
+        let response=await axios.get('/api/delivery-options?expand=estimatedDeviveryTime')
+            
                 setDeliveryOption(response.data)
-            })
-        axios.get('/api/payment-summary')
-            .then((response)=>{
+            
+        response = await axios.get('/api/payment-summary')
+            
                 setPaymentSummary(response.data)
-            })
-
+           
+        }
+        
+fetchChechoutData()
     },[])
 
 
