f8717d4a5137ab6d78aa23a5d2c5b92399cc17e1
Swathi Sudhakar Devadiga <sdswathi790@gmail.com>
2026-07-21 18:05:14 +0530

    Update HomePage tests

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -1,7 +1,7 @@
 import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import { HomePage } from './HomePage';
 
